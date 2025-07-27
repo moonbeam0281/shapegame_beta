a7577fdb099c031b32e4e002b0f9f7e05fd5631c
moonbeam0281 <moonbeam32145@gmail.com>
2025-07-27 13:27:32 +0200

    Updated

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+import pool from './db/pool.js';
+
+dotenv.config();
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use('/', (req, res) => {
+    res.send("hello world");
+});
+
+async function checkDb(){
+try {
+    const result = await pool.query('SELECT NOW()');
+    console.log('✅ Database connected at:', result.rows[0].now);
+  } catch (err) {
+    console.error('❌ Database connection error:', err);
+  }
+}
+
+checkDb();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
